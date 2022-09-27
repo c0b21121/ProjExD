25ad4802de3dc857aaf4ccc9bba972b38025eead
daichi <c0b2112110@edu.teu.ac.jp>
2022-09-27 18:15:36 +0900

    課題2

--- ex01/kadai01.py
+++ ex01/kadai01.py
@@ -1,24 +1,17 @@
 import random
 
-tai=["a","b","c","d","f"]
+tai=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 print("対象文字:",tai)
 
-hiy=random.sample(tai,3)
-print("表示文字:",random.sample(hiy,3))
+hiy=random.sample(tai,23)
+print("表示文字:",random.sample(hiy,23))
 
 keso=list(set(tai)-set(hiy))
 print("欠損文字:",keso)
 print(len(keso))
 inputword=(input(("欠損文字はいくつあるでしょうか?:")))
+
 if (int(inputword))==(len(keso)):
     print("正解")
 else:
     print("残念！")
-
-
-
-    
-
-
-
-

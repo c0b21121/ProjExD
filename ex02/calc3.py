4e492cb2fdd0319d4643281f757603cffe5b3b82
daichi <c0b2112110@edu.teu.ac.jp>
2022-10-04 15:47:23 +0900

    電卓final

---
+++ ex02/calc3.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def click_number(event): # 練習3
+    btn = event.widget
+    num = btn["text"]
+    #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+    entry.insert(tk.END, num) # 練習5
+def click_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res)
+
+root = tk.Tk() # 練習1
+root.geometry("300x500")
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
+entry.grid(row=0, column=0, columnspan=3)
+
+r, c = 1, 0 # r: 行を表す変数／c：列を表す変数
+numbers = list(range(9, -1, -1)) # 数字だけのリスト
+operators = ["+"] # 演算子だけのリスト
+for i, num in enumerate(numbers+operators, 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    btn.bind("<1>", click_number)
+    btn.grid(row=r, column=c)
+    c+=1
+    if i%3 == 0:
+        r += 1
+        c = 0
+btn = tk.Button(root, text=f"=", font=("", 30), width=4, height=2)
+btn.bind("<1>", click_equal)
+btn.grid(row=r, column=c)
+root.mainloop()

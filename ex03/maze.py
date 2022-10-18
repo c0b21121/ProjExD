90e4b5048c0c2b85eb4c528be9e9a022b2d43920
daichi <c0b2112110@edu.teu.ac.jp>
2022-10-18 15:34:35 +0900

    3限基本機能実装完了

---
+++ ex03/maze.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+
+import maze_maker as mm
+
+
+def key_down(event):
+    global key
+    key = event.keysym
+
+
+
+def key_up(event):
+    global key
+    key = ""
+
+
+def main_proc():
+    global cx, cy
+    global mx,my
+    
+    if key == "Up":
+        my -= 1
+    if key == "Down":
+        my += 1
+    if key == "Left":
+        mx -= 1
+    if key == "Right":
+        mx += 1
+    if maze_lst[my][mx]==0:
+        cx,cy=mx*100+50,my*100+50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
+
+    canv.coords("tori", cx, cy)
+    root.after(100, main_proc)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん") # 練習1
+
+    # 練習2
+    canv = tk.Canvas(root, width=1500, height=900, bg="black")
+    canv.pack()
+
+    maze_lst = mm.make_maze(15, 9)
+    # print(maze_lst) # 1:壁／0:床
+    mm.show_maze(canv, maze_lst)
+
+    # 練習3
+    tori = tk.PhotoImage(file="fig/5.png") 
+    mx,my=1,1
+    cx, cy = mx*100+50, my*100+50
+    canv.create_image(cx, cy, image=tori, tag="tori")
+
+    # 練習4
+    key = "" # 現在押されているキーを表す
+
+    # 練習5,6
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)    
+
+    # 練習7
+    main_proc()
+
+
+       
+    root.mainloop()

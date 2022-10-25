376e9bfdd3455e42f818017e5e127bbf87c8786f
daichi <c0b2112110@edu.teu.ac.jp>
2022-10-25 17:45:56 +0900

    爆弾ゲーム

---
+++ ex04/bomb.py
@@ -0,0 +1,110 @@
+import pygame as pg
+import sys
+from random import randint
+import tkinter.messagebox as tkm
+import tkinter as tk
+import datetime
+
+key_delta = {
+    pg.K_UP:    [0, -1],
+    pg.K_DOWN:  [0, +1],
+    pg.K_LEFT:  [-1, 0],
+    pg.K_RIGHT: [+1, 0],
+}
+
+def check_bound(obj_rct, scr_rct):
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: 
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: 
+        tate = -1
+    return yoko, tate
+
+def time():
+    pg.init()                                   
+    screen = pg.display.set_mode((300, 200))    
+    pg.display.set_caption("GAME")             
+    font = pg.font.Font(None, 25)               
+    while (1):
+        screen.fill((0,0,0))                                  
+        text = font.render(str(datetime.datetime.now()), True, (255,255,255))   
+        screen.blit(text, [20, 100])
+        pg.display.update()    
+        for event in pg.event.get():
+            if event.type == quit:  
+                pg.quit()       
+
+def main():
+    # 練習1
+    time()
+    pg.display.set_caption("逃げろ！こうかとん")
+    scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_rct = scrn_sfc.get_rect()
+    bg_sfc = pg.image.load("fig/veiw.jpg")
+    bg_rct = bg_sfc.get_rect()
+
+    # 練習3
+    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+
+    # 練習5
+    bomb_sfc = pg.Surface((20, 20)) # 空のSurface
+    bomb_sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10) # 円を描く
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = randint(0, scrn_rct.width)
+    bomb_rct.centery = randint(0, scrn_rct.height)
+    # 練習6
+    vx, vy = +1, +1
+
+
+    clock = pg.time.Clock() # 練習1
+    while True:
+        scrn_sfc.blit(bg_sfc, bg_rct) # 練習2
+        
+        for event in pg.event.get(): # 練習2
+            if event.type == pg.QUIT:
+                return
+
+        key_states = pg.key.get_pressed()
+        for key, delta in key_delta.items():
+            if key_states[key]:
+                tori_rct.centerx += delta[0]
+                tori_rct.centery += delta[1]
+                # 練習7
+                if check_bound(tori_rct, scrn_rct) != (+1, +1):
+                    tori_rct.centerx -= delta[0]
+                    tori_rct.centery -= delta[1]
+        scrn_sfc.blit(tori_sfc, tori_rct) # 練習3
+
+        # 練習7
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+        bomb_rct.move_ip(vx, vy) # 練習6
+        scrn_sfc.blit(bomb_sfc, bomb_rct) # 練習5
+
+        # 練習8
+        if tori_rct.colliderect(bomb_rct): # こうかとんrctが爆弾rctと重なったら
+            #main_surface = pg.display.set_mode((400, 700))
+            #font = pg.font.Font(None, 30)
+            #text_surface = font.render("game over", True, (0, 0, 255))
+            #main_surface.fill((220, 220, 220))
+            #main_surface.blit(text_surface, (100, 100))
+            tkm.showinfo("警告","game over!!!!!!!!!!")
+            return
+
+        pg.display.update() #練習2
+        clock.tick(1000)
+
+if __name__ == "__main__":
+    pg.init() # 初期化
+    main() # ゲームの本体
+    pg.quit() # 初期化の解除
+    sys.exit()
+
+
+
+        

ecbd978655f08acae619fd9b84678bc081be1af0
daichi <c0b2112110@edu.teu.ac.jp>
2022-11-15 18:20:11 +0900

    改定2

--- ex06/shooting.py
+++ ex06/shooting.py
@@ -97,9 +97,7 @@ def move_bullet(screen):#弾を飛ばす
     img_attack2 = pg.transform.rotozoom(img_attack1, 0, 1.0)
 
     for i in range(BULLET_MAX):
-        
         if bull_f[i] == True:
-
             bull_y[i] = bull_y[i] - 32
             screen.blit(img_attack2, [bull_x[i], bull_y[i]])
             if bull_y[i] < 0:
